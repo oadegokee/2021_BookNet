71b0cf9ad1333ba1b626ae41e8bec7a07c1ca925
oadegokee <oadegoke@student.cvtc.edu>
2021-11-09 15:17:00 -0600

    [finishes #180253441] Validating technology stack with test data

---
+++ js/bookNet.js
@@ -0,0 +1,14 @@
+$(function() {
+  
+  $('#websiteTitle').hover(function() {
+     $( "#websiteTitle" ).animate({
+      opacity: 0.25,
+      left: "+=50",
+      height: "toggle"
+    }, 5000, function() {
+    // Animation complete.
+    });
+  });
+
+});
+
