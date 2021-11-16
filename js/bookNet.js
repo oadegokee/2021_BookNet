48eef0bf98599be7c3e6e50ebf6fc58940e390a7
JKohls12 <89868836+JKohls12@users.noreply.github.com>
2021-11-16 15:11:01 -0600

    [Delivers #180254170] Adds title search bar
    Adds title search bar function
    Doesn't show all title

--- js/bookNet.js
+++ js/bookNet.js
@@ -1,7 +1,15 @@
 $(function() {
   
-  var api = "https://www.googleapis.com/books/v1/volumes?q=java";
-  
+  var api = "https://www.googleapis.com/books/v1/volumes?q=" + $(titleSrc);
+
+	var titleSrc = document.querySelector('.titleSrc').textContent;
+	var authorSrc = document.querySelector(".authorSrc").textContent;
+	var isbnSrc = document.querySelector(".isbnSrc").textContent;
+
+	var titleTxt = document.querySelector(".title").textContent;
+	var authorTxt = document.querySelector(".author").textContent;
+    
+    
   $("#submit").click(function() {
     
     fetch(api)
@@ -10,6 +18,10 @@ $(function() {
     })
     .then(function (data) {
       console.log(data);
+			title = data.items[0].volumeInfo.title;
+			console.log(title);
+      
+			$(titleTxt).text("Title: " + title);
       
     })
   })
