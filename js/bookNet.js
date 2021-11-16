d7d71f68e1a111cfb3b535db73a9ddee33d9a5e9
oadegokee <oadegoke@student.cvtc.edu>
2021-11-16 14:12:47 -0600

    [finishes #180334459] Book Net connects to the google books API

--- js/bookNet.js
+++ js/bookNet.js
@@ -1,6 +1,17 @@
 $(function() {
   
-
-
+  var api = "https://www.googleapis.com/books/v1/volumes?q=java";
+  
+  $("#submit").click(function() {
+    
+    fetch(api)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      
+    })
+  })
 });
 
