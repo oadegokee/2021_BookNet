f417241a3a2175eda0d0cdb0b5709c9f68adafeb
JKohls12 <89868836+JKohls12@users.noreply.github.com>
2021-11-16 20:07:55 -0600

    [Delivers #180335686] Allows users to see authors
    When users search for a title it will show the title and the author

--- js/bookNet.js
+++ js/bookNet.js
@@ -4,25 +4,26 @@ $(function() {
 
   $("#submit").click(function() {
     var titleSrc = $(".titleSrc").val();
+    var authorSrc = $(".authorSrc").val();
+    var isbnSrc = $(".isbnSrc").val();
     
-    if (titleSrc == "") {
-        alert("Please enter a title");
+    if ((titleSrc == "") && (authorSrc == "") && (isbnSrc == "")) {
+        alert("Please enter a title, author, or ISBN");
     } else {
       $.get(api + titleSrc, function(response) {
         console.log(response);
 
         for(i=0; i<response.items.length; i++) {
           title = response.items[i].volumeInfo.title;
+					author = response.items[i].volumeInfo.authors;
 
           console.log(title);
 
-//					$.each(title, function(names){
-//						
-//					});
-          $(".title").append("Title: " + title + "<br>");
+          $(".title").append("Title: " + title + "<br>author: " + author + "<br><br>");
         } // end of for loop
 
       }); // end of get function
+			
     }
     
   }) // end of onclick function
