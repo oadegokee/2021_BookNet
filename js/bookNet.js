eff667846eb22f592a79e1c0a91c3f40b04890df
bolson123 <bolson123@student.cvtc.edu>
2021-11-23 15:07:40 -0600

    [Delivers #180334378] This allows the user to search for a book by isbn.

--- js/bookNet.js
+++ js/bookNet.js
@@ -2,6 +2,7 @@ $(function() {
   
   var titleApi = "https://www.googleapis.com/books/v1/volumes?q=intitle:";
   var authorApi = "https://www.googleapis.com/books/v1/volumes?q=inauthor:";
+  var isbnApi = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
 
   $("#submit").click(function() {
     var titleSrc = $(".titleSrc").val();
@@ -45,6 +46,46 @@ $(function() {
 
       }); // end of get function
 			
+    }  else if (isbnSrc != "") {
+      // Search by isbn
+      $.get(isbnApi + authorSrc + isbnSrc, function(response) {
+        console.log(response);
+        $(".title").html("");
+
+        for(i=0; i<response.items.length; i++) {
+          title = response.items[i].volumeInfo.title;
+					author = response.items[i].volumeInfo.authors;
+          isbn = response.items[i].volumeInfo.industryIdentifiers[1].identifier;
+
+          console.log(author);
+          console.log(isbn);
+
+          $(".title").append("Title: " + title + "<br>Author: " + author + "<br>ISBN:" + isbn + "<br><br>");
+          
+        } // end of for loop
+
+      }); // end of get function
+			
+    } else if (authorSrc != "" && titleSrc != "") {
+      // api for both author and title
+      titleAndAuthorApi = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + titleSrc + "+inauthor:" + authorSrc;
+      // getting books by title.
+      $.get(titleAndAuthorApi, function(response) {
+        console.log(response);
+				$(".title").html("");
+
+        for(i=0; i<response.items.length; i++) {
+          title = response.items[i].volumeInfo.title;
+					author = response.items[i].volumeInfo.authors;
+
+          console.log(title);
+
+          $(".title").append("Title: " + title + "<br>Author: " + author + "<br><br>");
+          
+        } // end of for loop
+
+      }); // end of get function
+
     }
     
   }) // end of onclick function
